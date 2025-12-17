da21aeb46e1c4309f49b328e393c4810d407ab37
Никита Потапов <ns.potapov@yandex.ru>
2025-12-18 01:04:46 +0400

    рабочий вариант для частного случая

--- ontology_builder.py
+++ ontology_builder.py
@@ -1,16 +1,21 @@
 from owlready2 import *
+import types
 
 
 class OntologyBuilder:
     def __init__(self, iri="http://example.org/fuzzy.owl"):
         self.onto = get_ontology(iri)
 
-    def build(self, extracted_entities, fuzzy_mapper):
         with self.onto:
 
             class Concept(Thing):
                 pass
 
+            # Объявляем аннотационное свойство
+            class fuzzyDegree(AnnotationProperty):
+                pass
+
+    def build(self, extracted_entities, fuzzy_mapper):
         for item in extracted_entities:
             entity_name = item["entity"].capitalize()
             modifier = item["modifier"]
@@ -20,11 +25,13 @@ class OntologyBuilder:
                 continue
 
             with self.onto:
-                cls = types.new_class(entity_name, (Concept,))
-                cls.comment.append(
-                    f"Нечеткий модификатор: {modifier}, степень = {fuzzy_value}"
-                )
-                cls.fuzzyDegree = fuzzy_value
+                cls = types.new_class(entity_name, (self.onto.Concept,))
 
-    def save(self, path="fuzzy.owl"):
+                # Добавляем аннотацию
+                cls.fuzzyDegree.append(fuzzy_value)
+                cls.comment.append(
+                    f"Нечеткий модификатор: '{modifier}', степень принадлежности = {fuzzy_value}"
+                )
+
+    def save(self, path="data/fuzzy.owl"):
         self.onto.save(file=path, format="rdfxml")
