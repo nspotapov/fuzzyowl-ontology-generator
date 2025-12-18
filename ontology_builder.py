4b706a750156604f315063883592d840f132bb83
Никита Потапов <ns.potapov@yandex.ru>
2025-12-18 10:06:17 +0400

    пофиксил билдер

--- ontology_builder.py
+++ ontology_builder.py
@@ -21,17 +21,13 @@ class OntologyBuilder:
             class Quality(Thing):
                 pass
 
-            class FuzzyConcept(Thing):
-                pass
-
             class FuzzyRelation(Thing):
                 pass
 
-            # ---------- Object properties ----------
+            class FuzzyQualityRelation(Thing):
+                pass
 
-            class hasQuality(ObjectProperty):
-                domain = [Entity]
-                range = [Quality]
+            # ---------- Object properties ----------
 
             class hasSource(ObjectProperty):
                 domain = [FuzzyRelation]
@@ -41,20 +37,26 @@ class OntologyBuilder:
                 domain = [FuzzyRelation]
                 range = [Thing]
 
+            class fqHasSource(ObjectProperty):
+                domain = [FuzzyQualityRelation]
+                range = [Entity]
+
+            class fqHasQuality(ObjectProperty):
+                domain = [FuzzyQualityRelation]
+                range = [Quality]
+
             # ---------- Fuzzy annotation ----------
 
             class fuzzyDegree(AnnotationProperty):
                 pass
 
+        # ---------- Save references ----------
         self.Entity = self.onto.Entity
         self.Quality = self.onto.Quality
-        self.FuzzyConcept = self.onto.FuzzyConcept
         self.FuzzyRelation = self.onto.FuzzyRelation
+        self.FuzzyQualityRelation = self.onto.FuzzyQualityRelation
 
     def get_or_create_class(self, name, base):
-        """
-        Создаёт или возвращает OWL-класс в онтологии
-        """
         if name not in self.created_classes:
             with self.onto:
                 cls = type(name, (base,), {})
@@ -62,55 +64,55 @@ class OntologyBuilder:
         return self.created_classes[name]
 
     def build(self, extracted, fuzzy_mapper):
-        """
-        Строит онтологию на основе извлечённых сущностей и отношений
-        """
         for item in extracted:
+
+            modifier = item.get("modifier")
+            degree = fuzzy_mapper.map(modifier) if modifier else None
 
             # ---------- Entity ----------
             if item["type"] == "entity":
                 self.get_or_create_class(item["entity"].capitalize(), self.Entity)
 
-            # ---------- Fuzzy concept ----------
-            elif item["type"] == "fuzzy_concept":
-                degree = fuzzy_mapper.map(item["quality"])
-                if degree is None:
-                    continue
+            # ---------- Quality ----------
+            elif item["type"] == "quality":
+                self.get_or_create_class(item["quality"].capitalize(), self.Quality)
 
-                cls = self.get_or_create_class(
-                    f"{item['quality'].capitalize()}{item['entity'].capitalize()}",
-                    self.FuzzyConcept,
-                )
-                cls.fuzzyDegree.append(degree)
-
-            # ---------- Quality relation ----------
+            # ---------- Fuzzy quality relation ----------
             elif item["type"] == "relation":
                 from_cls = self.get_or_create_class(
                     item["from"].capitalize(), self.Entity
                 )
                 to_cls = self.get_or_create_class(item["to"].capitalize(), self.Quality)
-                from_cls.hasQuality.append(to_cls)
+
+                fq = self.FuzzyQualityRelation()
+                fq.fqHasSource.append(from_cls)
+                fq.fqHasQuality.append(to_cls)
+
+                if degree is not None:
+                    fq.fuzzyDegree.append(degree)
 
             # ---------- Fuzzy relation ----------
             elif item["type"] == "fuzzy_relation":
                 from_cls = self.get_or_create_class(
                     item["from"].capitalize(), self.Entity
                 )
-                to_cls = self.get_or_create_class(item["to"].capitalize(), self.Entity)
+
+                # цель может быть и Entity, и Quality
+                to_base = (
+                    self.Quality
+                    if item.get("target_type") == "quality"
+                    else self.Entity
+                )
+                to_cls = self.get_or_create_class(item["to"].capitalize(), to_base)
 
                 fr = self.FuzzyRelation()
                 fr.hasSource.append(from_cls)
                 fr.hasTarget.append(to_cls)
 
-                if item.get("modifier"):
-                    degree = fuzzy_mapper.map(item["modifier"])
-                    if degree is not None:
-                        fr.fuzzyDegree.append(degree)
+                if degree is not None:
+                    fr.fuzzyDegree.append(degree)
 
     def save(self, path=None):
-        """
-        Сохраняет онтологию в OWL-файл
-        """
         if path is None:
             path = f"data/fuzzy-{str(uuid.uuid4())[:8]}.owl"
 
